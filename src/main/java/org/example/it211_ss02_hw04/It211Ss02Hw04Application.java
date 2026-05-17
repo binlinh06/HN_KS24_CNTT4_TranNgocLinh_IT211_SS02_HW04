bf474417e7740957c8ba08bad8f79d1e72f1b4c6
KingDragon PC <nglinh150606@gmail.com>
2026-05-17 20:51:33 +0700

    done

---
+++ src/main/java/org/example/it211_ss02_hw04/It211Ss02Hw04Application.java
@@ -0,0 +1,13 @@
+package org.example.it211_ss02_hw04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Ss02Hw04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It211Ss02Hw04Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/it211_ss02_hw04/It211Ss02Hw04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.it211_ss02_hw04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It211Ss02Hw04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
